053ae8bfd6a52ffc532b89e461212503e45ecf07
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-13 17:47:09 +0530

    Practice Program 6

---
+++ String.indexOf.java
@@ -0,0 +1,11 @@
+// write a java program to detect double and triple spaces in a string
+public class DoubleTripple {
+
+	public static void main(String[] args) {
+     String myString="This string contains double and triple  spaces";
+     System.out.println(myString.indexOf("  "));
+     System.out.println(myString.indexOf("   "));
+    	 
+	}
+
+}
